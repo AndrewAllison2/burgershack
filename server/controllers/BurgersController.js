aeecf39521e61e567891b7e1fb8e8bc396bf3394
Andrew <andrewallison5211@gmail.com>
2023-07-10 15:22:11 -0600

    Basic Function Done

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -8,6 +8,9 @@ export class BurgersController extends BaseController {
     this.router
       .get('', this.getBurgers)
       .get('/:id', this.getBurgerById)
+      .post('', this.addBurger)
+      .delete('/:id', this.removeBurger)
+      .put('/:id', this.editBurger)
   }
 
   async getBurgers(req, res, next) {
@@ -27,6 +30,44 @@ export class BurgersController extends BaseController {
 
       const foundBurger = await burgersService.getBurgerById(burgerId)
       res.send(foundBurger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async addBurger(req, res, next) {
+    try {
+      const burgerData = req.body
+      const addedBurger = await burgersService.addBurger(burgerData)
+
+      res.send(addedBurger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async removeBurger(req, res, next) {
+    try {
+      const burgerId = req.params.id
+
+      await burgersService.removeBurger(burgerId)
+
+      res.send('That burger is gone!')
+
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  editBurger(req, res, next) {
+    try {
+      const burgerId = req.params.id
+
+      const burgerData = req.body
+
+      const updatedBurger = burgersService.editBurger(burgerId, burgerData)
+      res.send(updatedBurger)
+
     } catch (error) {
       next(error)
     }
--- server/services/BurgersService.js
+++ server/services/BurgersService.js
@@ -16,6 +16,32 @@ class BurgersService {
     return foundBurger
   }
 
+  async addBurger(burgerData) {
+    burgerData.id = burgers.length + 1
+
+    burgers.push(burgerData)
+
+    return burgerData
+  }
+
+  async removeBurger(burgerId) {
+    const foundIndex = burgers.findIndex(b => b.id == burgerId)
+
+    if (foundIndex == -1) {
+      throw new BadRequest(`${burgerId} was not a valid id`)
+    }
+    burgers.splice(foundIndex, 1)
+  }
+
+  async editBurger(burgerId, burgerData) {
+    let originalBurger = await this.getBurgerById(burgerId)
+
+    originalBurger.name = burgerData.name || originalBurger.name
+    originalBurger.price = burgerData.price || originalBurger.price
+
+    return originalBurger
+  }
+
 }
 
 
